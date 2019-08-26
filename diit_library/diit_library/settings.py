ab91d2ac51d67d13a8fc823fca3b374cbb979100
Yeasir Arafat Ratul <49812046+YeasirArafatRatul@users.noreply.github.com>
2019-08-26 23:31:43 +0600

    Update settings.py

--- diit_library/diit_library/settings.py
+++ diit_library/diit_library/settings.py
@@ -152,6 +152,6 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_USE_TLS = True  # TLS = Transport Layout Security
 EMAIL_HOST_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'diitdigital.library@gmail.com'
-EMAIL_HOST_PASSWORD = 'admin2428'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
